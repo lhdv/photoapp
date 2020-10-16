3f9a703aa7e577ae183043d7ead18d8bfa2aa580
LH <lh@lhdv.net>
2020-10-16 19:11:57 -0300

    Create Payment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise  :database_authenticatable, :registerable,
           :confirmable,
           :recoverable, :rememberable, :validatable
+  
+  has_one :payment
+  accepts_nested_attributes_for :payment
 end
