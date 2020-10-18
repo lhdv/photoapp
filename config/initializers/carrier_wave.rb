e43eff550bca6f8936a332d155adb5978a3ba09b
LH <lh@lhdv.net>
2020-10-17 23:13:12 -0300

    Change CarrierWave/Fog settings to upload to S3 - Region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,14 @@
 if Rails.env.production?
   CarrierWave.configure do |cfg|
+    cfg.fog_provider = 'fig/aws'
     cfg.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET_KEY']
+      aws_secret_access_key: ENV['S3_SECRET_KEY'],
+      region: 'sa-east-1'
     }
     cfg.fog_directory = ENV['S3_BUCKET']
     cfg.fog_public = false
+    cfg.storage = :fog
   end
 end
