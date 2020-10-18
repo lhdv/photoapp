25559ebba5b38f7c28f9db0fd9237af444e57459
LH <lh@lhdv.net>
2020-10-17 22:23:00 -0300

    Add image size manipulation and AWS S3 setup

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,14 @@
 class Image < ApplicationRecord
   belongs_to :user
-
   mount_uploader :picture, PictureUploader
+
+  validate :picture_size
+
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
